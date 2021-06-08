aac4545b834f603f8a69df83f572441f6a87249b
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-09 00:13:07 +0900

    postCSS-basic commit

---
+++ react-basic/postCSS-basic/src/components/button1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './button1.module.css';
+
+const button1 = () => {
+	return (
+		<div className={styles.button}>
+			<span className={styles.text}>Button1</span>
+		</div>
+	);
+};
+
+export default button1;
