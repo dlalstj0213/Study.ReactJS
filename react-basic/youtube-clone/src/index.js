60cd877ef27e02481b9c722d586bfba3e7afdbbe
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-11 23:11:54 +0900

    working navbar

--- react-basic/youtube-clone/src/index.js
+++ react-basic/youtube-clone/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './css/navbar.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import App from './app';
 
 ReactDOM.render(
