b8ec7bd69263c46d091da28aa1369fd99805b46a
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-12 22:32:06 +0900

    create VideoItem, VideoList comps

--- react-basic/youtube-clone/src/app.jsx
+++ react-basic/youtube-clone/src/app.jsx
@@ -1,8 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import './app.css';
 import Navbar from './components/navbar';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-	return <Navbar />;
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow',
+		};
+
+		fetch(
+			'https://www.googleapis.com/youtube/v3/search?key=AIzaSyAtL0ZummCG7z_ET2STLFCdPc5zojxW5h0&part=snippet&maxResults=25&q=bts&key=AIzaSyAtL0ZummCG7z_ET2STLFCdPc5zojxW5h0',
+			requestOptions
+		)
+			.then((response) => response.json())
+			.then((result) => setVideos(result.items))
+			.catch((error) => console.log('error', error));
+	}, []);
+
+	return <VideoList videos={videos} />;
 }
 
 export default App;
