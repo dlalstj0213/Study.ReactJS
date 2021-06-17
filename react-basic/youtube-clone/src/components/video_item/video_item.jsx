4a6001ef589ae2786ffcef3408ace99549ef63ff
dlalstj0213 <dlalstj0213@gmail.com>
2021-06-18 00:07:22 +0900

    create video detail

--- react-basic/youtube-clone/src/components/video_item/video_item.jsx
+++ react-basic/youtube-clone/src/components/video_item/video_item.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import styles from '../../css/video/video_item.module.css';
 
-const VideoItem = ({ video: { snippet: snip } }) => (
-	<li className={styles.container}>
-		<div className={styles.video}>
-			<img
-				className={styles.thumnail}
-				src={snip.thumbnails.medium.url}
-				alt="video thumnail"
-			/>
-			<div className={styles.metadata}>
-				<p className={styles.title}>{snip.title}</p>
-				<p className={styles.channel}>{snip.channerlTitle}</p>
+const VideoItem = ({
+	video,
+	video: { snippet: snip },
+	onVideoClick,
+	display,
+}) => {
+	const displayType = display === 'list' ? styles.list : styles.grid;
+
+	return (
+		<li
+			className={`${styles.container} ${displayType}`}
+			onClick={() => onVideoClick(video)}
+		>
+			<div className={styles.video}>
+				<img
+					className={styles.thumnail}
+					src={snip.thumbnails.medium.url}
+					alt="video thumnail"
+				/>
+				<div className={styles.metadata}>
+					<p className={styles.title}>{snip.title}</p>
+					<p className={styles.channel}>{snip.channerlTitle}</p>
+				</div>
 			</div>
-		</div>
-	</li>
-);
+		</li>
+	);
+};
 
 export default VideoItem;
